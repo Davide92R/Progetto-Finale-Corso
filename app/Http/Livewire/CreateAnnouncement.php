bc55fe8db18d42b7fcaed0d6d568ba517705e1c3
Chiara Boem <chiara.boem90@gmail.com>
2022-11-16 20:40:47 +0100

    Terminata user story 8 mi dà undefined variable nella zona revisor

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Livewire\Component;
 use App\Models\Announce;
 use App\Models\Category;
@@ -101,9 +102,12 @@ class CreateAnnouncement extends Component
                     $newFilename="announces/{$announce->id}";
                     $newImage=$announce->images()->create(['path'=>$image->store($newFilename,'public')]);
 
-                    dispatch(new ResizeImage($newImage->path, 400 , 300));
-                    dispatch(new GoogleVisionSafeSearch($newImage->id));
-                    dispatch(new GoogleVisionLabelImage($newImage->id));
+                    RemoveFaces::withChain([
+                    (new ResizeImage($newImage->path, 400 , 300)),
+                    (new GoogleVisionSafeSearch($newImage->id)),
+                    (new GoogleVisionLabelImage($newImage->id)),
+                    ])->dispatch($newImage->id);
+
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
